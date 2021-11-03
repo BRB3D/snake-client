6f7b4edff23055ec05dfc90ac789ba4b4983ba33
BRB3D <samuraix221@hotmail.com>
2021-11-03 14:50:32 -0400

    Input now receives an argument, this argument is the object from client. once its passed inside the function the connection gets passed the movements so you are able to play the snake game.

--- input.js
+++ input.js
@@ -1,16 +1,31 @@
-const setupInput = function() {//sets up the input for the user to be able to play.
+const { connect } = require("http2");
+let connection;
+const setupInput = function(conn) {//sets up the input for the user to be able to play.
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on('data', handleUserInput);
-  return stdin;
+  return conn;
 };
 
 const handleUserInput = function(key) {//first key is the exit key. otherwise the process will never end.
   // your code here
   if (key === '\u0003') {
     process.exit();
+  }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
   }
 };
 
