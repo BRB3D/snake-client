77873cab336df258e4ceebc2dfac0dd6852d96c0
BRB3D <samuraix221@hotmail.com>
2021-11-03 09:49:03 -0400

    add play.js in order to create a client for a snake game, and added conn.on(data)

---
+++ play.js
@@ -0,0 +1,19 @@
+
+const net = require('net');
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '192.168.2.40',// IP address here,
+    port: 50541,// PORT number here,
+  });
+  conn.on('data', (data) => {
+    console.log(`Servers says : ${data}`);
+  })
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
